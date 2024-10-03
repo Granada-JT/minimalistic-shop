7c66d5c356e51078d657eef880ee8e576249c1c3
Jomar Tabios Granada <jomart.granada@gmail.com>
2024-10-03 16:19:32 +0800

    feat: add loading state in products

--- src/components/UserView.js
+++ src/components/UserView.js
@@ -1,14 +1,14 @@
 import { Col, Container, Row } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
+import Spinner from "react-bootstrap/Spinner";
 import "../App.css";
 
-export default function UserView({ productsData }) {
+export default function UserView({ productsData, isLoading }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const productsArr = productsData.map((product) => {
-      //only render the active products since the route used is /all from product.js page
       if (product.isActive === true) {
         return <ProductCard productProp={product} key={product._id} />;
       } else {
@@ -16,22 +16,40 @@ export default function UserView({ productsData }) {
       }
     });
 
-    //set the products state to the result of our map function, to bring our returned product component outside of the scope of our useEffect where our return statement below can see.
     setProducts(productsArr);
   }, [productsData]);
 
   return (
     <>
-      {/*       <ProductSearch />
-      <ProductSearchByPrice /> */}
       <Container className="my-4">
-        <Row id="userViewRow">
-          {products.map((product, index) => (
-            <Col key={index} sm={12} md={6} lg={4} xl={3}>
-              {product}
-            </Col>
-          ))}
-        </Row>
+        {isLoading ? (
+          <Row
+            id="userViewRow"
+            className="
+              d-flex
+              justify-content-center
+              align-items-center
+            "
+          >
+            Loading Products
+            <Spinner
+              animation="border"
+              style={{
+                height: "40px",
+                width: "40px",
+                margin: "10px 0 0 10px",
+              }}
+            />
+          </Row>
+        ) : (
+          <Row id="userViewRow">
+            {products.map((product, index) => (
+              <Col key={index} sm={12} md={6} lg={4} xl={3}>
+                {product}
+              </Col>
+            ))}
+          </Row>
+        )}
       </Container>
     </>
   );
