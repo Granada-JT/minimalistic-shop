295b3bc690ff0ca296e3044b8d42ab0e69ffbc71
Jomar Tabios Granada <jomart.granada@gmail.com>
2024-10-03 19:12:19 +0800

    fix: change replace env import statement

--- scripts/replace-env.js
+++ scripts/replace-env.js
@@ -1,12 +1,12 @@
-import { sync } from "replace-in-file";
+const replace = require('replace-in-file');
 const options = {
-  files: "build/index.html",
+  files: 'build/index.html',
   from: /%REACT_APP_GA_M_ID%/g,
   to: process.env.REACT_APP_GA_M_ID,
 };
 
 try {
-  sync(options);
+  replace.sync(options);
 } catch (error) {
-  console.error("Error occurred:", error);
+  console.error('Error occurred:', error);
 }
