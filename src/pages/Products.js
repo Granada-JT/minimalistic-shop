bf4abde221dc2bf8235e88982adb79268c3a838f
Jomar Tabios Granada <75769590+Granada-JT@users.noreply.github.com>
2024-10-04 00:02:33 +0800

    Release v2.0.1 (#26)
    * feat: install eslint and prettier
    
    * chore: eslint and prettier initial run (auto-fix changes)
    
    * refactor: improve eslint config to accept jsx
    
    * chore: run eslint and prettier (manual-fix changes)
    
    * style: improve tab header icon and title
    
    * feat: add google tag
    
    * feat: add loading state in products
    
    * refactor: remove console logs
    
    * fix: delete manifest json and unused files
    
    * fix: add replace in file for id injection
    
    * fix: move script to src
    
    * fix: change name to scripts and move to root folder
    
    * fix: change build script directory for replace env
    
    * fix: change replace env import statement
    
    * fix: remove replace env to avoid build error
    
    * chore: uninstall replace in file
    
    * feat: install react ga4 module
    
    * fix: react ga4 integration
    
    * refactor: revert to initial gtag integration

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -6,13 +6,20 @@ import UserView from "../components/UserView";
 export default function Products() {
   const { user } = useContext(UserContext);
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const fetchData = () => {
-    fetch(`${process.env.REACT_APP_API_URL}/products/all`)
-      .then((res) => res.json())
-      .then((data) => {
-        setProducts(data);
-      });
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      fetch(`${process.env.REACT_APP_API_URL}/products/all`)
+        .then((res) => res.json())
+        .then((data) => {
+          setProducts(data);
+          setIsLoading(false);
+        });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   useEffect(() => {
@@ -24,7 +31,7 @@ export default function Products() {
       {user.isAdmin === true ? (
         <AdminView productsData={products} fetchData={fetchData} />
       ) : (
-        <UserView productsData={products} />
+        <UserView productsData={products} isLoading={isLoading} />
       )}
     </>
   );
