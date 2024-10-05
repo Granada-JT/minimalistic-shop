c9885a616a76036143feff2894d064cd3eaca576
Jomar Tabios Granada <jomart.granada@gmail.com>
2024-10-06 05:43:07 +0800

    feat: add product details page loading state

--- src/pages/ProductView.js
+++ src/pages/ProductView.js
@@ -9,17 +9,14 @@ import {
 } from "react-bootstrap";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
+import Spinner from "react-bootstrap/Spinner";
 import Swal from "sweetalert2";
 import UserContext from "../UserContext";
 import "../App.css";
 
 export default function ProductView() {
   const { user } = useContext(UserContext);
-
-  // The "useParams" hook allows us to retrieve the productId passed via the URL.
   const { productId } = useParams();
-
-  // an object with methods to redirect user.
   const navigate = useNavigate();
 
   const [name, setName] = useState("");
@@ -27,17 +24,27 @@ export default function ProductView() {
   const [price, setPrice] = useState(0);
   const [quantity, setQuantity] = useState(1);
   const [imgSrc, setImgSrc] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    // a fetch request that will retrieve the details of a specific product
-    fetch(`${process.env.REACT_APP_API_URL}/products/${productId}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setName(data.name);
-        setDescription(data.description);
-        setPrice(data.price);
-        setImgSrc(data.imgSrc);
-      });
+    const getProduct = async () => {
+      setIsLoading(true);
+      try {
+        fetch(`${process.env.REACT_APP_API_URL}/products/${productId}`)
+          .then((res) => res.json())
+          .then((data) => {
+            setName(data.name);
+            setDescription(data.description);
+            setPrice(data.price);
+            setImgSrc(data.imgSrc);
+            setIsLoading(false);
+          });
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getProduct();
   }, [productId]);
 
   // This code block is used to create an order and automatically add it to the cart.
@@ -92,93 +99,111 @@ export default function ProductView() {
 
   return (
     <Container>
-      <Row className="align-items-center" id="productViewCard">
-        <Col lg={6}>
-          <Card className="my-5 ms-lg-5 d-flex justify-content-center border-0">
-            <Card.Img variant="center" src={imgSrc} className="m-2" />
-          </Card>
-        </Col>
-        <Col lg={6} className="d-flex justify-content-center">
-          <Card className="my-5 me-lg-5 border-0">
-            <Card.Body>
-              <Card.Title className="py-2" style={{ fontSize: "2.5rem" }}>
-                {name}
-              </Card.Title>
-              <Card.Text
-                className="py-2"
-                style={{ fontSize: "1rem", textAlign: "justify" }}
-              >
-                {description}
-              </Card.Text>
-            </Card.Body>
-            <ListGroup>
-              <ListGroup.Item style={{ fontSize: "1.5rem", border: "none" }}>
-                Price: ₱{price}
-              </ListGroup.Item>
-              {/* Add more list items here if needed */}
-              <ListGroup.Item style={{ border: "none" }}>
-                <Form.Label>Quantity:</Form.Label>
-
-                {/* This code block does not allow any negative values and 0 to be entered into the quantity field. */}
-                <Form.Control
-                  type="text"
-                  pattern="[0-9]*"
-                  placeholder="Enter Quantity"
-                  required
-                  minLength={1}
-                  value={quantity}
-                  onChange={(e) => {
-                    const value = e.target.value;
-                    if (
-                      !value.includes("-") &&
-                      !value.includes("--") &&
-                      !value.includes("e") &&
-                      value >= 1
-                    ) {
-                      setQuantity(parseInt(value, 10));
-                    } else {
-                      setQuantity("");
-                    }
-                  }}
-                />
-              </ListGroup.Item>
-            </ListGroup>
-            <Row>
-              <Col>
-                <Card.Body
-                  style={{ display: "flex", justifyContent: "center" }}
+      {isLoading ? (
+        <Row
+          id="userViewRow"
+          className="
+            d-flex
+            justify-content-center
+            align-items-center
+          "
+        >
+          Loading Product Details
+          <Spinner
+            animation="border"
+            style={{
+              height: "40px",
+              width: "40px",
+              margin: "10px 0 0 10px",
+            }}
+          />
+        </Row>
+      ) : (
+        <Row className="align-items-center" id="productViewCard">
+          <Col lg={6}>
+            <Card className="my-5 ms-lg-5 d-flex justify-content-center border-0">
+              <Card.Img variant="center" src={imgSrc} className="m-2" />
+            </Card>
+          </Col>
+          <Col lg={6} className="d-flex justify-content-center">
+            <Card className="my-5 me-lg-5 border-0">
+              <Card.Body>
+                <Card.Title className="py-2" style={{ fontSize: "2.5rem" }}>
+                  {name}
+                </Card.Title>
+                <Card.Text
+                  className="py-2"
+                  style={{ fontSize: "1rem", textAlign: "justify" }}
                 >
-                  {user.id !== null ? (
-                    <Button
-                      type="submit"
-                      id="submitBtn"
-                      className="mx-2 btn-lg w-100 border-0"
-                      style={{ backgroundColor: "#3B638C" }}
-                      onClick={() => {
-                        order(productId);
-                        addToCart();
-                      }}
-                    >
-                      Add to Cart
-                    </Button>
-                  ) : (
-                    <Button
-                      type="submit"
-                      id="submitBtn"
-                      className="mx-2 btn-lg w-100 border-0"
-                      style={{ backgroundColor: "#3B638C" }}
-                      as={Link}
-                      to="/login"
-                    >
-                      Add to Cart
-                    </Button>
-                  )}
-                </Card.Body>
-              </Col>
-            </Row>
-          </Card>
-        </Col>
-      </Row>
+                  {description}
+                </Card.Text>
+              </Card.Body>
+              <ListGroup>
+                <ListGroup.Item style={{ fontSize: "1.5rem", border: "none" }}>
+                  Price: ₱{price}
+                </ListGroup.Item>
+                <ListGroup.Item style={{ border: "none" }}>
+                  <Form.Label>Quantity:</Form.Label>
+                  <Form.Control
+                    type="text"
+                    pattern="[0-9]*"
+                    placeholder="Enter Quantity"
+                    required
+                    minLength={1}
+                    value={quantity}
+                    onChange={(e) => {
+                      const value = e.target.value;
+                      if (
+                        !value.includes("-") &&
+                        !value.includes("--") &&
+                        !value.includes("e") &&
+                        value >= 1
+                      ) {
+                        setQuantity(parseInt(value, 10));
+                      } else {
+                        setQuantity("");
+                      }
+                    }}
+                  />
+                </ListGroup.Item>
+              </ListGroup>
+              <Row>
+                <Col>
+                  <Card.Body
+                    style={{ display: "flex", justifyContent: "center" }}
+                  >
+                    {user.id !== null ? (
+                      <Button
+                        type="submit"
+                        id="submitBtn"
+                        className="mx-2 btn-lg w-100 border-0"
+                        style={{ backgroundColor: "#3B638C" }}
+                        onClick={() => {
+                          order(productId);
+                          addToCart();
+                        }}
+                      >
+                        Add to Cart
+                      </Button>
+                    ) : (
+                      <Button
+                        type="submit"
+                        id="submitBtn"
+                        className="mx-2 btn-lg w-100 border-0"
+                        style={{ backgroundColor: "#3B638C" }}
+                        as={Link}
+                        to="/login"
+                      >
+                        Add to Cart
+                      </Button>
+                    )}
+                  </Card.Body>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </Container>
   );
 }
