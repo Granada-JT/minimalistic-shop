124cd8b6fc4d4501cd90410e95c9f6ad966909e6
Jomar Tabios Granada <jomart.granada@gmail.com>
2024-10-09 23:28:54 +0800

    fix: improve sign up form validation

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -18,8 +18,61 @@ export default function Register() {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [isActive, setIsActive] = useState(false);
 
+  const [errors, setErrors] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    mobileNo: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const validateForm = () => {
+    let valid = true;
+    let newErrors = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      mobileNo: "",
+      password: "",
+      confirmPassword: "",
+    };
+
+    if (firstName === "") {
+      newErrors.firstName = "First name is required.";
+      valid = false;
+    }
+    if (lastName === "") {
+      newErrors.lastName = "Last name is required.";
+      valid = false;
+    }
+    if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+      newErrors.email = "Please enter a valid email address.";
+      valid = false;
+    }
+    if (mobileNo.length < 1 || !/^\d+$/.test(mobileNo)) {
+      newErrors.mobileNo = "Please enter a valid mobile number";
+      valid = false;
+    }
+    if (password.length < 8) {
+      newErrors.password = "Password must be at least 8 characters long.";
+      valid = false;
+    }
+    if (confirmPassword !== password) {
+      newErrors.confirmPassword = "Passwords do not match.";
+      valid = false;
+    }
+
+    setErrors(newErrors);
+    return valid;
+  };
+
   function registerUser(e) {
     e.preventDefault();
+    if (!validateForm()) {
+      return;
+    }
+
     fetch(`${process.env.REACT_APP_API_URL}/users/checkEmail`, {
       method: "POST",
       headers: {
@@ -60,46 +113,24 @@ export default function Register() {
                 setMobileNo("");
                 setPassword("");
                 setConfirmPassword("");
+                setErrors({});
 
                 Swal.fire({
                   title: "Registration Successful",
                   icon: "success",
-                  text: `Welcome, ${firstName} !`,
-                });
-              } else if (!data.access) {
-                Swal.fire({
-                  title: "Registration Failed",
-                  icon: "error",
-                  text: "The mobile number you entered is already registered. Please log in or use a different mobile number.",
+                  text: `Welcome, ${firstName}!`,
                 });
               } else {
                 Swal.fire({
                   title: "Registration Failed",
                   icon: "error",
-                  text: "Sorry, there was an error during registration. Please try again later.",
+                  text: "The mobile number you entered is already registered. Please log in or use a different mobile number.",
                 });
               }
             });
         }
       });
   }
-
-  useEffect(() => {
-    if (
-      firstName !== "" &&
-      lastName !== "" &&
-      email !== "" &&
-      mobileNo !== "" &&
-      password !== "" &&
-      confirmPassword !== "" &&
-      password === confirmPassword &&
-      mobileNo.length === 11
-    ) {
-      setIsActive(true);
-    } else {
-      setIsActive(false);
-    }
-  }, [firstName, lastName, email, mobileNo, password, confirmPassword]);
 
   const data = {
     backgroundImage: bgImage,
@@ -108,99 +139,98 @@ export default function Register() {
       user.id !== null ? (
         <Navigate to="/products" />
       ) : (
-        <Form
-          onSubmit={(e) => registerUser(e)}
-          className="d-flex flex-column"
-        >
+        <Form noValidate onSubmit={registerUser} className="d-flex flex-column">
           <h2 className="my-5 text-center">Sign Up</h2>
-          <Form.Group className="mb-3" controlId="First Name">
+          <Form.Group className="mb-3" controlId="FirstName">
             <Form.Label>First Name</Form.Label>
             <Form.Control
               type="text"
               placeholder="Enter your first name"
-              required
               value={firstName}
-              onChange={(e) => {
-                setFirstName(e.target.value);
-              }}
+              onChange={(e) => setFirstName(e.target.value)}
+              isInvalid={!!errors.firstName}
             />
+            {errors.firstName && (
+              <Form.Text className="text-danger">{errors.firstName}</Form.Text>
+            )}
           </Form.Group>
-          <Form.Group className="mb-3" controlId="Last Name">
+          <Form.Group className="mb-3" controlId="LastName">
             <Form.Label>Last Name</Form.Label>
             <Form.Control
               type="text"
               placeholder="Enter your last name"
-              required
               value={lastName}
-              onChange={(e) => {
-                setLastName(e.target.value);
-              }}
+              onChange={(e) => setLastName(e.target.value)}
+              isInvalid={!!errors.lastName}
             />
+            {errors.lastName && (
+              <Form.Text className="text-danger">{errors.lastName}</Form.Text>
+            )}
           </Form.Group>
-          <Form.Group className="mb-3" controlId="Email address">
-            <Form.Label>Email address</Form.Label>
+          <Form.Group className="mb-3" controlId="Email">
+            <Form.Label>Email</Form.Label>
             <Form.Control
               type="email"
-              placeholder="Enter your email e.g.(name@example.com)"
-              required
+              placeholder="Enter your email"
               value={email}
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
+              onChange={(e) => setEmail(e.target.value)}
+              isInvalid={!!errors.email}
+                  pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
             />
+            {errors.email && (
+              <Form.Text className="text-danger">{errors.email}</Form.Text>
+            )}
           </Form.Group>
-          <Form.Group className="mb-3" controlId="Mobile No">
-            <Form.Label>Mobile No:</Form.Label>
+          <Form.Group className="mb-3" controlId="MobileNo">
+            <Form.Label>Mobile No</Form.Label>
             <Form.Control
               type="text"
-              placeholder="Enter your 11 digit mobile no."
-              required
+              placeholder="Enter your 11-digit mobile number"
               value={mobileNo}
-              onChange={(e) => {
-                setMobileNo(e.target.value);
-              }}
+              onChange={(e) => setMobileNo(e.target.value)}
+              isInvalid={!!errors.mobileNo}
             />
+            {errors.mobileNo && (
+              <Form.Text className="text-danger">{errors.mobileNo}</Form.Text>
+            )}
           </Form.Group>
-          <Form.Group className="mb-3" controlId="Password1">
+          <Form.Group className="mb-3" controlId="Password">
             <Form.Label>Password</Form.Label>
             <Form.Control
               type="password"
               placeholder="Enter your password"
-              required
               value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
+              onChange={(e) => setPassword(e.target.value)}
+              isInvalid={!!errors.password}
             />
+            {errors.password && (
+              <Form.Text className="text-danger">{errors.password}</Form.Text>
+            )}
           </Form.Group>
-          <Form.Group className="mb-3" controlId="Password2">
-            <Form.Label>Confirm Password:</Form.Label>
+          <Form.Group className="mb-3" controlId="ConfirmPassword">
+            <Form.Label>Confirm Password</Form.Label>
             <Form.Control
               type="password"
               placeholder="Confirm your password"
-              required
               value={confirmPassword}
-              onChange={(e) => {
-                setConfirmPassword(e.target.value);
-              }}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              isInvalid={!!errors.confirmPassword}
             />
+            {errors.confirmPassword && (
+              <Form.Text className="text-danger">
+                {errors.confirmPassword}
+              </Form.Text>
+            )}
           </Form.Group>
           {isActive ? (
-            <Button
-              variant="primary"
-              type="submit"
-              id="submitBtn"
-              className="mb-5"
-            >
+            <Button variant="primary" type="submit" id="submitBtn" className="mb-5">
               Sign Up
             </Button>
           ) : (
             <Button
-              variant="danger"
               type="submit"
               id="submitBtn"
               className="mb-5"
-              disabled
             >
               Sign Up
             </Button>
